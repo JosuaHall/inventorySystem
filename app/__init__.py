ffffd2dc9b33b4d2cc65d942135622be26ea59e8
JosuaHall <halljosua@web.de>
2023-02-02 21:23:55 -0600

    bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # adding configuration for using a sqlite database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ifa16A2019nD%21@localhost/inventorySystem'
